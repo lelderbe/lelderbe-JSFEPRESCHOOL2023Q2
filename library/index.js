c12d53fa5765a6f1ca11523dda013a71f053e3b8
lelderbe <fvdc@ya.ru>
2023-08-06 20:57:06 +0300

    feat: add Burger-menu, self-check

--- library/index.js
+++ library/index.js
@@ -1,10 +1,23 @@
-console.log(`
-Score: 100 / 100
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerMenuBtn = document.querySelector('#menu-button');
+    const menu = document.querySelector('#menu');
 
-Вёрстка валидная (10/10)
-Вёрстка семантическая (16/16)
-Вёрстка соответствует макету (54/54)
-Общие требования к верстке (20/20)
+    if (!burgerMenuBtn || !menu) {
+        return;
+    }
+
+    burgerMenuBtn.addEventListener('click', (e) => {
+        e.target.classList.toggle('icon_burger-close');
+        menu.classList.toggle('navigation_show');
+    });
+});
+
+console.log(`
+Score: 50 / 50
+
+1. Вёрстка соответствует макету. Ширина экрана 768px (26/26)
+2. Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется (12/12)
+3. На ширине экрана 768рх реализовано адаптивное меню (12/12)
 
   ____________________________
 <  Я эксперт в своей области.  >
